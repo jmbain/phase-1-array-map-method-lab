257c40e62838046001191afe99e3afacf5d8f8f0
Josh Bain <joshuambain@gmail.com>
2024-07-20 17:35:25 -0400

    complete array map method lab

--- index.js
+++ index.js
@@ -12,5 +12,40 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return capitalizeFirstLetter(tutorials)
 }
+
+
+// Attempt 1
+// const titleCased = {
+//   for (let i = 0; i < tutorials.length; i++) {
+//     const tutorialWords = tutorials[i].split(" ");
+//     for (let w = 0; w < tutorialWords.length; w++) {
+//       return tutorialWords[0].toLocaleUpperCase()
+//     }
+//   }
+// }
+
+// Attempt 2
+// const titleCased = tutorials.map(tutorial => {
+//   const words = tutorial.split(" ");
+//   const titleCaseWords = words.map(word => {
+//     return word.charAt(0).toUpperCase() + word.slice(1)
+//   });
+//   return titleCaseWords.join(" ");
+// })
+
+// 3rd Attempt
+function capitalizeFirstLetter(tutorials) {
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(" ");
+    const titleCasedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return titleCasedWords.join(" ");
+  });
+}
+
+
+// Revisit...
+// const titleCased2 = tutorials.map(tutorial => tutorial.split(" "))
